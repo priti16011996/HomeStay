fddf6d17c989dfd6e4122d40455072dc35383b05
Priti <priti16maurya@gmail.com>
2026-04-04 04:19:45 +0530

    Create Review form for my HomeStay Project in mern stack

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const MONGO_URL ="mongodb://127.0.0.1:27017/HomeStay";
 const path = require('path');
 const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const Review = require("./models/review.js")
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -31,6 +34,7 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+
 //Home Route
 app.get("/",(req,res)=>{
     res.send("Hi!, Welcome to Home Stay")
@@ -53,52 +57,91 @@ app.get("/",(req,res)=>{
 //     res.send("Listing Route")
 // });
 
-app.get("/Listings",async(req,res)=>{
+app.get("/Listings",wrapAsync(async(req,res)=>{
     let HomeStayListings = await Listing.find();
    // console.log(HomeStayListting);
    // res.send(HomeStayListting);
    res.render("listings/index.ejs",{HomeStayListings});
-});
+}));
 
 app.get("/Listings/addNew",(req,res)=>{
     res.render("listings/AddHomeStay.ejs");
 })
 
-app.get("/Listings/:id",async(req,res)=>{
+app.get("/Listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let specificHomeStayData = await Listing.findById(id);
    // console.log(specificHomeStayData);
    res.render("listings/show.ejs",{specificHomeStayData});
-});
+}));
+// Without try catch block phase-1
+// app.put("/Listings", async(req,res)=>{
+//     let listingData = new Listing(req.body.listing);
+//     await listingData.save();
+//     res.redirect("/Listings");
+// });
 
-app.put("/Listings", async(req,res)=>{
+// With try catch block phase-2
+// app.put("/Listings", async(req,res,next)=>{
+//     try{
+//         let listingData = new Listing(req.body.listing);
+//         await listingData.save();
+//         res.redirect("/Listings");
+//     }catch(err) {
+//         next(err);
+//     }
+    
+// });
+// With wrapAsync phase-3
+app.put("/Listings", wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Invalid Listing Data");
+    }
     let listingData = new Listing(req.body.listing);
     await listingData.save();
     res.redirect("/Listings");
-});
+}));
 
-app.post("/Listings/:id/editHomeStayDesc", async(req, res) => {
+app.post("/Listings/:id/editHomeStayDesc", wrapAsync(async(req, res) => {
     let { id } = req.params;
     let HomeStayData = await Listing.findById(id);
     res.render("listings/EditHomeStayInfo.ejs", { HomeStayData }); 
-});
+}));
 
 
-app.put("/Listings/:id", async(req,res)=>{
+app.put("/Listings/:id", wrapAsync(async(req,res)=>{
     let {id} = req.params;
+    if(!req.body.listing){
+        throw new ExpressError(400,"Invalid Listing Data");
+    }
     let HomeStayData = await Listing.findByIdAndUpdate(id,req.body.listing);
-    console.log(HomeStayData);
+    //console.log(HomeStayData);
     res.redirect(`/Listings/${id}`);
-});
+}));
 
-app.delete("/Listings/:id", async(req,res)=>{
+app.delete("/Listings/:id", wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let HomeStayData = await Listing.findByIdAndDelete(id);
-    console.log(HomeStayData);
+    //console.log(HomeStayData);
     res.redirect("/Listings");
+}));
+
+app.post("/Listings/:id/reviews", async(req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let NewReview = await new Review(req.body.review);
+    listing.reviews.push(NewReview);
+    await NewReview.save();
+    await listing.save();
+    res.redirect(`/Listings/${id}`);
+
+})
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 });
-
-
+app.use((err,req,res,next)=>{
+    let {statusCode,message} = err;
+    res.status(statusCode).render("errors", { message, statusCode });
+})
 
 
 
